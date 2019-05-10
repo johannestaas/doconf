2b167051888e107fec3d820f446726dd1d4fb63d
Johan Nestaas <johannestaas@gmail.com>
2019-05-10 15:46:23 -0700

    better format, add to README

--- README.rst
+++ README.rst
@@ -1,7 +1,7 @@
 doconf
 ======
 
-Configuration specified through documentation, supporting multiple formats.
+Configuration specified through documentation.
 
 Installation
 ------------
@@ -10,16 +10,28 @@ From the project root directory::
 
     $ python setup.py install
 
+Or from pip::
+
+    $ pip install doconf
+
 Usage
 -----
-
-Simply run it::
-
-    $ doconf
 
 Use --help/-h to view info on the arguments::
 
     $ doconf --help
+
+Find will show you where the config would be loaded from in the current environment::
+
+    $ doconf find examples.my_example_app.config:CustomConfig
+
+Validate will find your config and parse it, tell you whether it has all required variables and show you the values::
+
+    $ doconf validate examples.my_example_app.config:CustomConfig --config-path examples/my_example_app/my_example_app.cfg
+
+Generate will dump example configuration files for you to provide as examples::
+
+    $ doconf generate examples.my_example_app.config:CustomConfig --out .
 
 Release Notes
 -------------
--- doconf/cli.py
+++ doconf/cli.py
@@ -1,12 +1,12 @@
 import os
+import sys
 
 
 def load_class(module, class_name):
     submod = __import__(module)
     for next_mod in module.split('.')[1:]:
         submod = getattr(submod, next_mod)
-    cls = getattr(submod, class_name)
-    return cls
+    return getattr(submod, class_name)
 
 
 def main():
@@ -15,23 +15,49 @@ def main():
     subs = parser.add_subparsers(dest='cmd')
 
     s = subs.add_parser('find')
-    s.add_argument('module', help='module containing class, eg: my_app.config')
-    s.add_argument('class_name', help='the class with the doc string')
+    s.add_argument(
+        'class_path',
+        help=(
+            'path to the module and class, '
+            'eg: custom_example.config:CustomConfig'
+        ),
+    )
 
     s = subs.add_parser('validate')
-    s.add_argument('module', help='module containing class, eg: my_app.config')
-    s.add_argument('class_name', help='the class with the doc string')
+    s.add_argument(
+        'class_path',
+        help=(
+            'path to the module and class, '
+            'eg: custom_example.config:CustomConfig'
+        ),
+    )
     s.add_argument('--config-path', '-c', help='direct path to config')
     s.add_argument(
         '--env', '-e', default='default', help='the environment to use',
     )
 
     s = subs.add_parser('generate')
+    s.add_argument(
+        'class_path',
+        help=(
+            'path to the module and class, '
+            'eg: custom_example.config:CustomConfig'
+        ),
+    )
 
     args = parser.parse_args()
 
+    if ':' not in getattr(args, 'class_path', ''):
+        parser.print_usage()
+        print()
+        print('You need to pass the class name in the class path, like: ')
+        print('my_app.config:MyConfigClass')
+        sys.exit(1)
+    else:
+        module, class_name = args.class_path.split(':', 1)
+        cls = load_class(module, class_name)
+
     if args.cmd == 'find':
-        cls = load_class(args.module, args.class_name)
         paths = cls.possible_paths()
         first = None
         print('Would look at these paths, and found these files:\n')
@@ -46,7 +72,6 @@ def main():
         else:
             print('Would have loaded: {}'.format(first))
     elif args.cmd == 'validate':
-        cls = load_class(args.module, args.class_name)
         conf = cls.load(path=args.config_path, env=args.env)
         for sect_name in conf._values.keys():
             sect_title = 'Section {!r}'.format(sect_name)
