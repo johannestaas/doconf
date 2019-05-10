e241b1c8b898c3aab6a1beb2c093beb183c10489
Johan Nestaas <johannestaas@gmail.com>
2019-05-10 11:28:00 -0700

    test envs

--- tests/test_doconf.py
+++ tests/test_doconf.py
@@ -5,7 +5,12 @@ import pytest
 rootdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path = [rootdir] + sys.path
 
-from doconf import DoconfConfig, DoconfBadConfigError
+from doconf import (
+    DoconfConfig,
+    DoconfBadConfigError,
+    DoconfClassError,
+    DoconfTypeError,
+)
 
 
 class BasicConfig(DoconfConfig):
@@ -77,6 +82,14 @@ def test_case_insensitivity():
     assert conf['SectioN1']['NamE'] == 'joey'
     assert conf['section1']['NaMe'] == 'joey'
 
+    assert 'name' in conf['section1']
+    assert 'NAME' in conf['section1']
+    assert 'NaMe' in conf['section1']
+
+    assert 'section1' in conf
+    assert 'SECTION1' in conf
+    assert 'sectION1' in conf
+
 
 def test_all_defaults():
     conf = BasicConfig.load(text='''
@@ -100,3 +113,99 @@ def test_all_defaults():
 def test_missing_required():
     with pytest.raises(DoconfBadConfigError):
         BasicConfig.load(text='''[second_section]''')
+
+
+def test_config_missing_name():
+    with pytest.raises(DoconfClassError):
+        class BadConfig(DoconfConfig):
+            '''
+
+            {DEFAULT}
+
+            [section1]
+            HOST (str:"127.0.0.1"): the hostname
+            '''
+            pass
+
+
+def test_config_missing_env():
+    with pytest.raises(DoconfClassError):
+        class BadConfig(DoconfConfig):
+            '''
+            name: myapp
+            [section1]
+            HOST (str:"127.0.0.1"): the hostname
+            '''
+            pass
+
+
+def test_config_missing_section():
+    with pytest.raises(DoconfClassError):
+        class BadConfig(DoconfConfig):
+            '''
+            name: myapp
+            {DEFAULT}
+            HOST (str:"127.0.0.1"): the hostname
+            '''
+            pass
+
+
+def test_config_class_has_bad_type_default():
+    with pytest.raises(DoconfTypeError):
+        class BadConfig(DoconfConfig):
+            '''
+            name: myapp
+            {DEFAULT}
+            [section1]
+            PORT (int:"foo"): foo is not a number
+            '''
+            pass
+    with pytest.raises(DoconfTypeError):
+        class BadConfig2(DoconfConfig):
+            '''
+            name: myapp
+            {DEFAULT}
+            [section1]
+            PROGRESS (float:"foo"): foo is not a float
+            '''
+            pass
+
+
+def test_multiple_envs():
+    class GoodConfig(DoconfConfig):
+        '''
+        name: my_server
+
+        {DEFAULT}
+        [server]
+        PORT (int:8080): server hosted on this port
+        DEBUG (bool:true): debugging mode activate
+
+        {STAGING}
+        [server]
+        PORT (int:8081): this port
+        HOSTNAME (str:"staging.example.org"): this host
+
+        {PRODUCTION}
+        [server]
+        PORT (int:8082): this port
+        HOSTNAME (str:"production.example.org"): this production host
+        DEBUG (bool:false): not debugging in prod
+        '''
+        pass
+
+    default_conf = GoodConfig.load(env='default', text='')
+    staging_conf = GoodConfig.load(env='staging', text='')
+    prod_conf = GoodConfig.load(env='production', text='')
+
+    assert default_conf['server']['port'] == 8080
+    assert default_conf['server']['debug'] is True
+    assert 'HOSTNAME' not in default_conf['server']
+
+    assert staging_conf['server']['port'] == 8081
+    assert staging_conf['server']['hostname'] == 'staging.example.org'
+    assert 'DEBUG' not in staging_conf['server']
+
+    assert prod_conf['server']['port'] == 8082
+    assert prod_conf['server']['hostname'] == 'production.example.org'
+    assert prod_conf['server']['debug'] is False
