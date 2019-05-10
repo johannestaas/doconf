33cbe512c4163be79ca8352fa9f2fe8253063da8
Johan Nestaas <johannestaas@gmail.com>
2019-05-10 15:31:01 -0700

    validate

--- doconf/cli.py
+++ doconf/cli.py
@@ -19,6 +19,12 @@ def main():
     s.add_argument('class_name', help='the class with the doc string')
 
     s = subs.add_parser('validate')
+    s.add_argument('module', help='module containing class, eg: my_app.config')
+    s.add_argument('class_name', help='the class with the doc string')
+    s.add_argument('--config-path', '-c', help='direct path to config')
+    s.add_argument(
+        '--env', '-e', default='default', help='the environment to use',
+    )
 
     s = subs.add_parser('generate')
 
@@ -39,6 +45,19 @@ def main():
             print('None found!')
         else:
             print('Would have loaded: {}'.format(first))
+    elif args.cmd == 'validate':
+        cls = load_class(args.module, args.class_name)
+        conf = cls.load(path=args.config_path, env=args.env)
+        for sect_name in conf._values.keys():
+            sect_title = 'Section {!r}'.format(sect_name)
+            sect_title = '{}\n{}'.format(sect_title, '-' * len(sect_title))
+            print(sect_title)
+            sect = conf[sect_name]
+            for key, val in sorted(sect.items()):
+                print('{} ({}) = {!r}'.format(
+                    key, val.__class__.__name__, val,
+                ))
+            print()
     else:
         parser.print_usage()
 
